0420436617557d9e18d96284e8da942b78026b57
sgsayani <sgsayanighatak@gmail.com>
2025-09-14 13:21:11 +0530

    chnges

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useAppContext } from '../Context/AppContext'
 
 const Sidebar = () => {
+  const {chats,setselectedChat,user,theme, setTheme} = useAppContext()
+  
   return (
     <div>
       sidebar
